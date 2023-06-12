0da3b4b17b77084a5947fd55d889b1a579be0f55
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-12 18:08:17 -0400

    add color flipper take 1 app js

--- 01-color-flipper/take1/app.js
+++ 01-color-flipper/take1/app.js
@@ -1,1 +1,19 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+//color flipper
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+
+//target elements
+const btn = document.querySelector('#btn');
+const main = document.querySelector('main');
+const colorName = document.querySelector('.color');
+
+//event listner
+btn.addEventListener('click', function () {
+  const number = randomNumber();
+  main.style.backgroundColor = colors[number];
+  colorName.textContent = colors[number];
+});
+
+//functions
+function randomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
