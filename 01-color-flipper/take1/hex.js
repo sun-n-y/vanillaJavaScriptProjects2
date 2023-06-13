7ba8cd4c47622235fe4da1f577ee71f2f14b7be7
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-13 17:32:46 -0400

    add color flipper take 1 hex js

--- 01-color-flipper/take1/hex.js
+++ 01-color-flipper/take1/hex.js
@@ -1,10 +1,20 @@
+//
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
-for (i = 1; i < 7; i++) {
-  let string = '#';
-  string += string;
-  console.log(string);
-}
+// target elements
+const btn = document.querySelector('#btn');
+const colorValue = document.querySelector('.color');
+const main = document.querySelector('main');
+
+//event listener
+btn.addEventListener('click', function () {
+  let colorString = '#';
+  for (i = 1; i < 7; i++) {
+    colorString += hex[randomNumber()];
+  }
+  colorValue.textContent = colorString;
+  main.style.background = colorString;
+});
 
 //functions
 function randomNumber() {
