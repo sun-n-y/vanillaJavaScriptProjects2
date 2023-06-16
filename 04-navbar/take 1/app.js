0788ef6a237056925c0dba0680f157d4242bb13f
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-16 11:58:48 -0400

    add navbar take 1 html and app js

--- 04-navbar/take 1/app.js
+++ 04-navbar/take 1/app.js
@@ -3,3 +3,12 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+//target elements
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+//event listeners
+navToggle.addEventListener('click', function () {
+  links.classList.toggle('show-links');
+});
