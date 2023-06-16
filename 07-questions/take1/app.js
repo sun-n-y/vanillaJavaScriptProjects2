0794c1dc276c2d37ee5bb47f6593816916627880
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-16 16:57:01 -0400

    add questions take 1 html app js

--- 07-questions/take1/app.js
+++ 07-questions/take1/app.js
@@ -1,2 +1,24 @@
 //using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function (question) {
+  const btn = question.querySelector('.question-btn');
+  btn.addEventListener('click', function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove('show-text');
+      }
+    });
+    question.classList.toggle('show-text');
+  });
+});
+
 // traversing the dom
+// const questionBtns = document.querySelectorAll('.question-btn');
+
+// questionBtns.forEach(function (btn) {
+//   btn.addEventListener('click', function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text');
+//   });
+// });
