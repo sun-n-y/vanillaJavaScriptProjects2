3cd7f1acab999a058c8219edf7e0a65117cc5533
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-16 15:14:30 -0400

    add modal take 1 html and app js

--- 06-modal/take 1/app.js
+++ 06-modal/take 1/app.js
@@ -2,3 +2,17 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+//target elements
+const openBtn = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+//event listener
+openBtn.addEventListener('click', function () {
+  modalOverlay.classList.add('open-modal');
+});
+
+closeBtn.addEventListener('click', function () {
+  modalOverlay.classList.remove('open-modal');
+});
