b493e0f832316801f7a331060c42b2e88c12f3e4
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-17 17:24:12 -0400

    add video take 1 html and app js

--- 09-video/take1/app.js
+++ 09-video/take1/app.js
@@ -1,3 +1,22 @@
 // MDN
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+//target elements
+const btn = document.querySelector('.switch-btn');
+const video = document.querySelector('.video-container');
+const preloader = document.querySelector('.preloader');
+
+//event listener
+btn.addEventListener('click', function () {
+  btn.classList.toggle('slide');
+  if (btn.classList.contains('slide')) {
+    video.pause();
+  } else {
+    video.play();
+  }
+});
+
+window.addEventListener('load', function () {
+  preloader.classList.add('hide-preloader');
+});
