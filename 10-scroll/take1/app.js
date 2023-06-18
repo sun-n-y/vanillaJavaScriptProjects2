9b4d6a6466e05a397ea9c579dae6a11ac5330a97
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-17 22:52:52 -0400

    add scroll take 1 smooth scroll app js

--- 10-scroll/take1/app.js
+++ 10-scroll/take1/app.js
@@ -4,10 +4,63 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = document.querySelector('.date');
+
+date.innerText = new Date().getFullYear();
 
 // ********** close links ************
+//target elements
+const linksContainer = document.querySelector('.links-container');
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
 
+//event listener
+navToggle.addEventListener('click', function () {
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
 // ********** fixed navbar ************
+const navbar = document.querySelector('#nav');
+const topLink = document.querySelector('.top-link');
+
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+window.addEventListener('scroll', function () {
+  if (scrollY > navbarHeight * 0.8) {
+    navbar.classList.add('fixed-nav');
+  } else {
+    navbar.classList.remove('fixed-nav');
+  }
+  if (scrollY > 400) {
+    topLink.classList.add('show-link');
+  } else {
+    topLink.classList.remove('show-link');
+  }
+});
 
 // ********** smooth scroll ************
-// select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = e.currentTarget.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+    if (!navbar.classList.contains('fixed-nav')) {
+      scrollTo({
+        left: 0,
+        top: element.offsetTop - navbarHeight - navbarHeight,
+      });
+    } else {
+      scrollTo({
+        left: 0,
+        top: element.offsetTop - navbarHeight,
+      });
+    }
+  });
+});
