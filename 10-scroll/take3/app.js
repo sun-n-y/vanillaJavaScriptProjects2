462f6219ee57a4f5b25b8ecb298fead93000d5b0
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-20 11:51:34 -0400

    add scroll take 3 fixed navbar js

--- 10-scroll/take3/app.js
+++ 10-scroll/take3/app.js
@@ -6,9 +6,41 @@
 // ********** set date ************
 const date = document.querySelector('.date');
 date.innerHTML = new Date().getFullYear();
+
 // ********** close links ************
+const navBtn = document.querySelector('.nav-toggle');
+const container = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+const linksHeight = links.getBoundingClientRect().height;
+
+navBtn.addEventListener('click', function () {
+  const containerHeight = container.getBoundingClientRect().height;
+
+  if (containerHeight == 0) {
+    container.style.height = `${linksHeight}px`;
+  } else {
+    container.style.height = 0;
+  }
+});
 
 // ********** fixed navbar ************
+const navbar = document.querySelector('#nav');
+const navbarHeight = navbar.getBoundingClientRect().height;
+const topBtn = document.querySelector('.top-link');
+
+window.addEventListener('scroll', function () {
+  if (scrollY > navbarHeight * 0.8) {
+    navbar.classList.add('fixed-nav');
+  } else {
+    navbar.classList.remove('fixed-nav');
+  }
+
+  if (scrollY > 400) {
+    topBtn.classList.add('show-link');
+  } else {
+    topBtn.classList.remove('show-link');
+  }
+});
 
 // ********** smooth scroll ************
 // select links
