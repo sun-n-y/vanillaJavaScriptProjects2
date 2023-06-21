3f3836edc95882ab6425c74221f82d39cdf08675
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-21 10:16:24 -0400

    add tabs take 1 apps js compelte

--- 11-tabs/take1/app.js
+++ 11-tabs/take1/app.js
@@ -1,18 +1,25 @@
 //target elements
 const tabBtns = document.querySelectorAll('.tab-btn');
+const contentText = document.querySelectorAll('.content');
 
 //event listener
 tabBtns.forEach(function (btn) {
   btn.addEventListener('click', function (e) {
     const tab = e.currentTarget.dataset.id;
-    console.log(tab);
     tabBtns.forEach(function (item) {
       if (tab === item.dataset.id) {
         item.classList.add('active');
       } else {
         item.classList.remove('active');
       }
-      console.log(item);
+    });
+    contentText.forEach(function (tabContent) {
+      const attr = tabContent.getAttribute('id');
+      if (tab === attr) {
+        tabContent.classList.add('active');
+      } else {
+        tabContent.classList.remove('active');
+      }
     });
   });
 });
