737f427a4c641dac0734e4f57470978e5ecb4f3c
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-22 12:10:29 -0400

    add counter take 1 app js complete

--- 12-countdown-timer/take1/app.js
+++ 12-countdown-timer/take1/app.js
@@ -32,7 +32,7 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let futureDate = new Date(2023, 5, 22, 13, 0, 0);
+let futureDate = new Date(2023, 5, 22, 12, 10, 0);
 // let futureDate = new Date();
 
 const year = futureDate.getFullYear();
@@ -55,3 +55,45 @@ if (futureDate.getHours() >= 12) {
 }
 
 giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hour}:${minute}${ampm}`;
+
+//future time in mili seconds
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  const oneSecond = 1000;
+
+  const days = Math.floor(t / oneDay);
+  const hours = Math.floor((t % oneDay) / oneHour);
+  const mins = Math.floor((t % oneHour) / oneMinute);
+  const secs = Math.floor((t % oneMinute) / oneSecond);
+
+  const times = [days, hours, mins, secs];
+
+  function format(item) {
+    if (item < 10) {
+      item = `0${item}`;
+      return item;
+    } else {
+      return item;
+    }
+  }
+
+  items.forEach(function (item, index) {
+    item.innerHTML = format(times[index]);
+  });
+
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired" > Sorry, giveaway has ended</h4>`;
+  }
+}
+
+let countdown = setInterval(getRemainingTime, 1000);
+
+getRemainingTime();
