9f94f0df9b99a262957c0b67a0bc79d77319a26e
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-22 11:05:37 -0400

    add counter take 1 deadline date app js

--- 12-countdown-timer/take1/app.js
+++ 12-countdown-timer/take1/app.js
@@ -1,23 +1,57 @@
 const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
 ];
 const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
 ];
+
+const hours = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+  12,
+];
+
+//target elements
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2023, 5, 22, 13, 0, 0);
+// let futureDate = new Date();
+
+const year = futureDate.getFullYear();
+const month = months[futureDate.getMonth()];
+const weekday = weekdays[futureDate.getDay()];
+const date = futureDate.getDate();
+let hour = hours[futureDate.getHours()];
+let minute = futureDate.getMinutes();
+
+let ampm;
+
+if (minute < 10) {
+  minute = `0${minute}`;
+}
+
+if (futureDate.getHours() >= 12) {
+  ampm = 'pm';
+} else {
+  ampm = 'am';
+}
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hour}:${minute}${ampm}`;
