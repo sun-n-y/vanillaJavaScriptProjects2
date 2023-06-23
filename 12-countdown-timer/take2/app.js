d3285dd53bd67758e11335da9fc9184d98a1af5d
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-23 09:36:24 -0400

    add countdown take 1

---
+++ 12-countdown-timer/take2/app.js
@@ -0,0 +1,28 @@
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const weekdays = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+const hours = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+  12,
+];
