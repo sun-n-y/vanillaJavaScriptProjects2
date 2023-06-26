6273b0bf36b8c5daa55bc84396ad2f11021282bc
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-26 16:05:40 -0400

    to do list take 2 html and app js

--- 14-grocery-bud/take2/app.js
+++ 14-grocery-bud/take2/app.js
@@ -1,10 +1,43 @@
 // ****** SELECT ITEMS **********
+const form = document.querySelector('.grocery-form');
+const alert = document.querySelector('.alert');
+const groceryInput = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
 
 // edit option
+let editElement;
+let editFlag = false;
+let editID = '';
 
 // ****** EVENT LISTENERS **********
+form.addEventListener('submit', addItem);
 
 // ****** FUNCTIONS **********
+function addItem(e) {
+  e.preventDefault();
+  const value = groceryInput.value;
+  const id = new Date().getTime().toString();
+  if (value && !editFlag) {
+    console.log('add new item');
+  } else if (value && editFlag) {
+    console.log('edit item');
+  } else {
+    displayAlert('please enter value', 'danger');
+  }
+}
+
+//display alert funciton
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  setTimeout(function () {
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
 
 // ****** LOCAL STORAGE **********
 
