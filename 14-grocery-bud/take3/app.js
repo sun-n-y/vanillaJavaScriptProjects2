1889587fdd879da8f7b614d9d30cd39f04892ddb
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-26 21:36:29 -0400

    add to do list 3 first scencario

--- 14-grocery-bud/take3/app.js
+++ 14-grocery-bud/take3/app.js
@@ -1,11 +1,66 @@
 // ****** SELECT ITEMS **********
+const form = document.querySelector('.grocery-form');
+const alert = document.querySelector('.alert');
+const itemInput = document.querySelector('#grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
 
 // edit option
+let editElement;
+let editFlag = false;
+let editID = '';
 
 // ****** EVENT LISTENERS **********
+form.addEventListener('submit', addItem);
 
 // ****** FUNCTIONS **********
+function addItem(e) {
+  e.preventDefault();
+  const value = itemInput.value;
+  const id = new Date().getTime().toString();
+  if (value && !editFlag) {
+    const element = document.createElement('article');
+    element.classList.add('grocery-item');
+    const attr = document.createAttribute('data-id');
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `<p class="title">${value}</p>
+          <div class="btn-container">
+            <button type="button" class="edit-btn"><i class="fas fa-edit"></i></button>
+            <button type="button" class="delete-btn"><i class="fas fa-trash"></i></button>
+          </div>`;
+    list.appendChild(element);
+    displayAlert('item added', 'success');
+    container.classList.add('show-container');
+    addToLocaleStorage(id, value);
+    setBackToDefault();
+  } else if (value && editFlag) {
+    console.log('edit');
+  } else {
+    displayAlert('please enter value', 'danger');
+  }
+}
+
+//display alert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  setTimeout(function () {
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+  }, 1500);
+}
+
+//set back to default
+function setBackToDefault() {
+  console.log('back to default');
+}
 
 // ****** LOCAL STORAGE **********
+function addToLocaleStorage(id, value) {
+  console.log('item added to locale storage');
+}
 
 // ****** SETUP ITEMS **********
