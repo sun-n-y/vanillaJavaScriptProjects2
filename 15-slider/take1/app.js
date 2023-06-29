0a86543cff6b61c71435302cb320451fc1c51e1f
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-29 10:35:33 -0400

    add slider take 1 slider approach 2

--- 15-slider/take1/app.js
+++ 15-slider/take1/app.js
@@ -23,13 +23,26 @@ prevBtn.addEventListener('click', function () {
 //funciton
 function carousel() {
   //working with slides
-  if (counter === slides.length) {
-    counter = 0;
+  //   if (counter === slides.length) {
+  //     counter = 0;
+  //   }
+  //   if (counter < 0) {
+  //     counter = slides.length - 1;
+  //   }
+  //working with buttons
+  if (counter < slides.length - 1) {
+    nextBtn.style.display = 'block';
+  } else {
+    nextBtn.style.display = 'none';
   }
-  if (counter < 0) {
-    counter = slides.length - 1;
+  if (counter > 0) {
+    prevBtn.style.display = 'block';
+  } else {
+    prevBtn.style.display = 'none';
   }
   slides.forEach(function (slide) {
     slide.style.transform = `translateX(-${counter * 100}%)`;
   });
 }
+
+prevBtn.style.display = 'none';
