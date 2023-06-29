4394baf2edb6c82cc91354695eaa4b61bee8cadd
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-29 10:17:32 -0400

    add slider take 1 slider js setup

---
+++ 15-slider/take1/app.js
@@ -0,0 +1,8 @@
+//target element
+const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('.nextBtn');
+const prevBtn = document.querySelector('.prevBtn');
+
+slides.forEach(function (slide, index) {
+  slide.style.left = `${index * 100}%`;
+});
