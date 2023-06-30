8b2a7c2d4d9e8bddad5664b24ed063541a346fc3
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-30 16:25:17 -0400

    add counter oop app js stop

---
+++ 16-counter/take 1/app.js
@@ -0,0 +1,47 @@
+//get element function
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  }
+  throw new Error(
+    `please check "${selection}" selector, no such element exists`
+  );
+}
+
+//using constructor funciton
+
+//constructor funciton
+function Counter(element, value) {
+  this.counter = element;
+  this.value = value;
+  this.resetBtn = element.querySelector('.reset');
+  this.increaseBtn = element.querySelector('.increase');
+  this.decreaseBtn = element.querySelector('.decrease');
+  this.valueDOM = element.querySelector('.value');
+  this.valueDOM.textContent = this.value;
+  //bind this to all funciton
+  this.increase = this.increase.bind(this);
+  this.decrease = this.decrease.bind(this);
+  this.reset = this.reset.bind(this);
+
+  this.increaseBtn.addEventListener('click', this.increase);
+  this.decreaseBtn.addEventListener('click', this.decrease);
+  this.resetBtn.addEventListener('click', this.reset);
+}
+
+Counter.prototype.increase = function () {
+  this.value++;
+  this.valueDOM.textContent = this.value;
+};
+Counter.prototype.decrease = function () {
+  this.value--;
+  this.valueDOM.textContent = this.value;
+};
+Counter.prototype.reset = function () {
+  this.value = 0;
+  this.valueDOM.textContent = this.value;
+};
+
+const firstCounter = new Counter(getElement('.first-counter'), 100);
+const secondCounter = new Counter(getElement('.second-counter'), 200);
