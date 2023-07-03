e7287ad23f8e57323c3e3a98426108b12a784f9b
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-03 11:44:04 -0400

    add counter setup

--- 16-counter/take 1/app.js
+++ 16-counter/take 1/app.js
@@ -1,47 +1,22 @@
-//get element function
+//constructor funciton
+function Counter(element, value) {
+  console.log(element, value);
+}
+
+const firstCounter = new Counter(getElement('.first-counter'), 100);
+const secondCounter = new Counter(getElement('.second-counter'), 200);
+
+console.log(firstCounter);
+console.log(secondCounter);
+
+//function to get element
 function getElement(selection) {
   const element = document.querySelector(selection);
   if (element) {
     return element;
+  } else {
+    throw new Error(
+      `Please check "${selection}" selector, no such element exists`
+    );
   }
-  throw new Error(
-    `please check "${selection}" selector, no such element exists`
-  );
 }
-
-//using constructor funciton
-
-//constructor funciton
-function Counter(element, value) {
-  this.counter = element;
-  this.value = value;
-  this.resetBtn = element.querySelector('.reset');
-  this.increaseBtn = element.querySelector('.increase');
-  this.decreaseBtn = element.querySelector('.decrease');
-  this.valueDOM = element.querySelector('.value');
-  this.valueDOM.textContent = this.value;
-  //bind this to all funciton
-  this.increase = this.increase.bind(this);
-  this.decrease = this.decrease.bind(this);
-  this.reset = this.reset.bind(this);
-
-  this.increaseBtn.addEventListener('click', this.increase);
-  this.decreaseBtn.addEventListener('click', this.decrease);
-  this.resetBtn.addEventListener('click', this.reset);
-}
-
-Counter.prototype.increase = function () {
-  this.value++;
-  this.valueDOM.textContent = this.value;
-};
-Counter.prototype.decrease = function () {
-  this.value--;
-  this.valueDOM.textContent = this.value;
-};
-Counter.prototype.reset = function () {
-  this.value = 0;
-  this.valueDOM.textContent = this.value;
-};
-
-const firstCounter = new Counter(getElement('.first-counter'), 100);
-const secondCounter = new Counter(getElement('.second-counter'), 200);
