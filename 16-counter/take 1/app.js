2ecc8affaec944dd3edc0cb5f2e23d767ab20ba3
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-03 12:22:12 -0400

    add functions to prototype

--- 16-counter/take 1/app.js
+++ 16-counter/take 1/app.js
@@ -1,3 +1,15 @@
+//helper function: to get target element
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  } else {
+    throw new Error(
+      `Please check "${selection}" selector, no such element exists`
+    );
+  }
+}
+
 //constructor funciton
 function Counter(element, value) {
   this.counter = element;
@@ -9,20 +21,21 @@ function Counter(element, value) {
   this.valueDOM.textContent = value;
 }
 
+//add functions to prototype
+Counter.prototype.increase = function () {
+  console.log(this);
+  this.value++;
+  this.valueDOM.textContent = this.value;
+};
+Counter.prototype.decrease = function () {
+  this.value--;
+  this.valueDOM.textContent = this.value;
+};
+Counter.prototype.reset = function () {
+  this.value = 0;
+  this.valueDOM.textContent = 0;
+};
+
+//create instances
 const firstCounter = new Counter(getElement('.first-counter'), 100);
 const secondCounter = new Counter(getElement('.second-counter'), 200);
-
-console.log(firstCounter);
-console.log(secondCounter);
-
-//function to get element
-function getElement(selection) {
-  const element = document.querySelector(selection);
-  if (element) {
-    return element;
-  } else {
-    throw new Error(
-      `Please check "${selection}" selector, no such element exists`
-    );
-  }
-}
