22fd2a838c621db9f7364757ee5a9a3eaa3eda95
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-04 18:26:24 -0400

    add gallery select modal setup

--- 17-gallery/take2/app.js
+++ 17-gallery/take2/app.js
@@ -1,3 +1,4 @@
+//helper function, to get target element
 function getElement(selection) {
   const element = document.querySelector(selection);
   if (element) {
@@ -7,3 +8,35 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+//constructor function to select specific gallery
+function Gallery(element) {
+  this.container = element;
+  this.list = [...element.querySelectorAll('.img')];
+  this.modal = getElement('.modal');
+  this.closeBtn = getElement('.close-btn');
+  this.prevBtn = getElement('.prev-btn');
+  this.nextBtn = getElement('.next-btn');
+  this.mainImage = getElement('.main-img');
+  this.imageName = getElement('.image-name');
+  this.modalImages = getElement('.modal-images');
+  element.addEventListener(
+    'click',
+    function (e) {
+      if (e.target.classList.contains('img')) {
+        this.openModal();
+      }
+    }.bind(this)
+  );
+}
+
+//prototype function
+Gallery.prototype.openModal = function () {
+  console.log('hi');
+};
+
+//instances
+const nature = new Gallery(getElement('.nature'));
+const city = new Gallery(getElement('.city'));
+
+console.log(nature);
