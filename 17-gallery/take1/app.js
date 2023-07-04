99b817978f032a1915969496759cf8ecc39132b1
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-04 11:57:01 -0400

    add gallery take 1 select elements

--- 17-gallery/take1/app.js
+++ 17-gallery/take1/app.js
@@ -1,3 +1,4 @@
+//helper function, to get target elements
 function getElement(selection) {
   const element = document.querySelector(selection);
   if (element) {
@@ -7,3 +8,23 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+//constructor function to look for section(element) of nature or city
+function Gallery(element) {
+  //selecting all images sitting inside seciton
+  //use spread operator to turn node list into an array
+  this.list = [...element.querySelectorAll('.img')];
+  //target modal
+  this.modal = getElement('.modal');
+  this.modalImg = getElement('.main-img');
+  this.modalImages = getElement('.modal-images');
+  this.closeBtn = getElement('.close-btn');
+  this.nextBtn = getElement('.next-btn');
+  this.prevBtn = getElement('.prev-btn');
+}
+
+//create instances
+const nature = new Gallery(getElement('.nature'));
+const city = new Gallery(getElement('.city'));
+
+console.log(nature);
