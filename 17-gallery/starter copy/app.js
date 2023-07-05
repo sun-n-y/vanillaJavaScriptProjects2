24f03003888503f92ae22113cc0d4d199c2a64ee
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-05 10:45:36 -0400

    add gallery take 3 open modal

--- 17-gallery/starter copy/app.js
+++ 17-gallery/starter copy/app.js
@@ -1,3 +1,4 @@
+//helper function, to get target element
 function getElement(selection) {
   const element = document.querySelector(selection);
   if (element) {
@@ -7,3 +8,38 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+//constructor function
+function Gallery(element) {
+  this.container = element;
+  this.list = [...element.querySelectorAll('.img')];
+  this.modal = getElement('.modal');
+  this.closebtn = getElement('.close-btn');
+  this.prevBtn = getElement('.prev-btn');
+  this.nextBtn = getElement('.next-btn');
+  this.mainImage = getElement('.main-img');
+  this.imageName = getElement('.image-name');
+  this.modalImages = getElement('.modal-images');
+  //bind functions
+  //event listener for section
+  this.container.addEventListener(
+    'click',
+    function (e) {
+      if (e.target.classList.contains('img')) {
+        this.openModal(e.target, this.list);
+      }
+    }.bind(this)
+  );
+}
+
+//prototypes for constructor
+Gallery.prototype.openModal = function (selected, list) {
+  this.modal.classList.add('open');
+  console.log(selected, list);
+};
+
+//instances
+const nature = new Gallery(getElement('.nature'));
+const city = new Gallery(getElement('.city'));
+
+console.log(nature);
