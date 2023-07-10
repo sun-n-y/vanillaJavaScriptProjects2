c888923198532b67270e8148448ad6bad1ac6602
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-10 13:32:41 -0400

    add numbers take 2 app js complete

--- 18-numbers/starter copy/app.js
+++ 18-numbers/starter copy/app.js
@@ -1,1 +1,21 @@
-console.log('numbers project');
+//target elements
+const items = [...document.querySelectorAll('.number')];
+
+//for each
+const updateCounter = items.forEach((item) => {
+  increaseCount(item);
+});
+
+//function
+function increaseCount(el) {
+  const value = el.dataset.value;
+  const increment = Math.ceil(value / 1000);
+  let intialValue = 0;
+  const counter = setInterval(() => {
+    intialValue += increment;
+    if (intialValue >= value) {
+      clearInterval(counter);
+    }
+    el.textContent = `${intialValue}+`;
+  }, 1);
+}
