1a2765cee6c59d4bc6ad092e7c2a2aa76c8b7336
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-10 15:14:41 -0400

    add dark mode take app js complete

--- 19-dark-mode/take1/app.js
+++ 19-dark-mode/take1/app.js
@@ -11,11 +11,12 @@ toggleBtn.addEventListener('click', () => {
 const articlesData = articles
   .map((article) => {
     const { title, date, length, snippet } = article;
+    const formatDate = moment(date).format('MMMM Do, YYYY');
     return `<article class="post">
         <h2>${title}</h2>
         <div class="post-info">
-          <span>${date}</span>
-          <span>${length}</span>
+          <span>${formatDate}</span>
+          <span>${length} min read</span>
         </div>
         <p>${snippet}</p>
       </article>`;
