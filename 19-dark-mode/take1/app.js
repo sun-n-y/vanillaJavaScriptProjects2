93679e21d2b1546d93ced412b0ee5088217f562c
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-10 14:58:51 -0400

    add dark mode take app js pt 2

--- 19-dark-mode/take1/app.js
+++ 19-dark-mode/take1/app.js
@@ -1,7 +1,25 @@
 //target elements
 const toggleBtn = document.querySelector('.btn');
+const articlesContainer = document.querySelector('.articles');
 
 //event listener
 toggleBtn.addEventListener('click', () => {
   document.documentElement.classList.toggle('dark-theme');
 });
+
+//data import
+const articlesData = articles
+  .map((article) => {
+    const { title, date, length, snippet } = article;
+    return `<article class="post">
+        <h2>${title}</h2>
+        <div class="post-info">
+          <span>${date}</span>
+          <span>${length}</span>
+        </div>
+        <p>${snippet}</p>
+      </article>`;
+  })
+  .join('');
+
+articlesContainer.innerHTML = articlesData;
