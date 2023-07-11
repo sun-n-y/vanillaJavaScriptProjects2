198596c8b568f1bf6af0798cd9142e3c2cf1c76a
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-10 22:16:23 -0400

    add filters take 2 app js pt3

--- 20-filters/take2/app.js
+++ 20-filters/take2/app.js
@@ -1,10 +1,14 @@
-// - setup a function displayProducts, iterate over products, display them
-
+//display products
 let filteredProducts = [...products];
 
 const productContainer = document.querySelector('.products-container');
 
 const displayProducts = () => {
+  //empty Array
+  if (filteredProducts.length < 1) {
+    productContainer.innerHTML = `<h6>Sorry, product not found</h6>`;
+    return;
+  }
   productContainer.innerHTML = filteredProducts
     .map(({ id, title, image, price }) => {
       return `<article class="product" data-id="${id}">
@@ -19,3 +23,36 @@ const displayProducts = () => {
 };
 
 displayProducts();
+
+//search based on text
+const form = document.querySelector('.input-form');
+const searchInput = document.querySelector('.search-input');
+
+form.addEventListener('keyup', () => {
+  const inputValue = searchInput.value.toLowerCase();
+  filteredProducts = products.filter((product) => {
+    return product.title.toLowerCase().includes(inputValue);
+  });
+  displayProducts();
+});
+
+//display filter buttons
+const btnContainer = document.querySelector('.companies');
+
+const displayButtons = () => {
+  const buttons = [
+    'All',
+    ...new Set(
+      products.map((product) => {
+        return product.company;
+      })
+    ),
+  ];
+  btnContainer.innerHTML = buttons
+    .map((button) => {
+      return `<button class="company-btn">${button}</button>`;
+    })
+    .join('');
+};
+
+displayButtons();
