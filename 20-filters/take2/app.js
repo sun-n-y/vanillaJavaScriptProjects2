c0c53927f6afd7d8adfe3bd2b833e7d36a03a856
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-10 21:05:08 -0400

    add filters take 2 app js pt1

--- 20-filters/take2/app.js
+++ 20-filters/take2/app.js
@@ -1,1 +1,21 @@
-console.log('filters project');
+// - setup a function displayProducts, iterate over products, display them
+
+let filteredProducts = [...products];
+
+const productContainer = document.querySelector('.products-container');
+
+const displayProducts = () => {
+  productContainer.innerHTML = filteredProducts
+    .map(({ id, title, image, price }) => {
+      return `<article class="product" data-id="${id}">
+        <img src="${image}" alt="" class="product-img img">
+        <footer>
+          <h5 class="product-name">${title}</h5>
+          <span class="product-price">$${price}</span>
+        </footer>
+      </article>`;
+    })
+    .join('');
+};
+
+displayProducts();
