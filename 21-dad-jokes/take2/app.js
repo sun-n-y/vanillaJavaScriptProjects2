90f6f6f37233c37d776e9b40f122f006da4dea31
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-14 11:45:07 -0400

    add dad jokes take 2 complete

--- 21-dad-jokes/take2/app.js
+++ 21-dad-jokes/take2/app.js
@@ -1,1 +1,29 @@
-console.log('Dad Jokes Starter');
+//dad jokes
+const url = 'https://icanhazdadjoke.com/';
+const btn = document.querySelector('.btn');
+const result = document.querySelector('.result');
+
+btn.addEventListener('click', () => {
+  fetchDadJoke();
+});
+
+const fetchDadJoke = async () => {
+  result.textContent = 'Loading...';
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: 'application/json',
+        'User-Agent': 'learning App',
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Error...`);
+    }
+    const data = await response.json();
+    result.textContent = data.joke;
+  } catch (error) {
+    result.textContent = 'Error...';
+  }
+};
+
+fetchDadJoke();
