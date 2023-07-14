f39cec165bd3431f2f9d951087075d8cbb952ecc
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-14 15:28:42 -0400

    add products take 1 fetch products and display them

--- 22-products/take1/app.js
+++ 22-products/take1/app.js
@@ -2,6 +2,7 @@
 const url = 'https://course-api.com/javascript-store-products';
 const productsDOM = document.querySelector('.products-center');
 
+//this function is responsible for getting data
 const fetchProduct = async () => {
   productsDOM.innerHTML = `<div class="loading"></div>`;
   try {
@@ -13,10 +14,29 @@ const fetchProduct = async () => {
   }
 };
 
+//this fucntion is responsible for displaying data
 const displayProducts = (list) => {
-  console.log(list);
+  const productList = list
+    .map((product) => {
+      //pull desired data pieces
+      //id,name, price, image
+      const { id } = product;
+      const { name: title, price } = product.fields;
+      const formatPrice = price / 100;
+      const { url: img } = product.fields.image[0];
+      return `<a href="product.html" class="single-product">
+          <img src="${img}" alt="${title}" class="single-product-img img">
+          <footer>
+            <h5 class="name">${title}</h5>
+            <span class="price">$${formatPrice}</span>
+          </footer>
+        </a>`;
+    })
+    .join('');
+  productsDOM.innerHTML = `<div class="products-container">${productList}</div>`;
 };
 
+//this fucntion is responsible for modifying data
 const start = async () => {
   const data = await fetchProduct();
   displayProducts(data);
