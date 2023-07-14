3080972a73fb2fb6718a9803c1984d7b9b1735c5
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-14 16:44:54 -0400

    add products take 1 complete

--- 22-products/take1/product.js
+++ 22-products/take1/product.js
@@ -7,7 +7,6 @@ const fetchProduct = async () => {
     productDOM.innerHTML = '<h4 class="product-loading">Loading...</h4>';
     const params = new URLSearchParams(window.location.search);
     const id = params.get('id');
-    console.log(id);
     const response = await fetch(`${url}?id=${id}`);
     const data = await response.json();
     return data;
@@ -30,15 +29,18 @@ const displayProduct = (product) => {
   //title of page/tab
   document.title = title.toUpperCase();
   //colors
+  const colorsList = colors
+    .map((color) => {
+      return `<span class="product-color" style="background:${color}"></span>`;
+    })
+    .join('');
   productDOM.innerHTML = `<div class="product-wrapper">
             <img src="${img}" class="img" alt="${title}">
             <div class="product-info">
                 <h3>${title}</h3>
                 <h5>${company}</h5>
                 <span>${price / 100}</span>
-                <div class="colors">
-                    <span class="product-color"></span>
-                    <span class="product-color"></span>
+                <div class="colors">${colorsList}
                 </div>
                 <p>${description}</p>
                 <button class="btn">add to cart</button>
