f87788e65cca1a6169ec75d81468a591920c04e7
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-15 11:11:06 -0400

    add products take2 fetch products

--- 22-products/take2/app.js
+++ 22-products/take2/app.js
@@ -1,1 +1,22 @@
-console.log('products starter');
+const url = 'https://course-api.com/javascript-store-products';
+const productDOM = document.querySelector('.products-center');
+
+const fetchProducts = async () => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayProducts(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const displayProducts = (products) => {
+  console.log(products);
+};
+
+const start = () => {
+  fetchProducts();
+};
+
+start();
