41ba35d838f11a841d9316f7215570b3c29beb3a
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-18 13:57:19 -0400

    add random user take 1 complete

--- 23-random-user/take1/app.js
+++ 23-random-user/take1/app.js
@@ -1,17 +1,12 @@
 import get from './utils/getElement.js';
 import getUser from './utils/fetchUser.js';
+import displayUser from './utils/displayUser.js';
 
-const img = get('.user-img');
-const title = get('.user-title');
-const value = get('.user-value');
 const btn = get('.btn');
-const btns = [...document.querySelectorAll('.icon')];
 
 const showUser = async () => {
-  //get user from api
   const person = await getUser();
-  console.log(person);
-  //display user info in btns
+  displayUser(person);
 };
 
 window.addEventListener('DOMContentLoaded', showUser);
---
+++ 23-random-user/take1/utils/removeActive.js
@@ -0,0 +1,7 @@
+function removeActive(items) {
+  items.forEach((btn) => {
+    btn.classList.remove('active');
+  });
+}
+
+export default removeActive;
