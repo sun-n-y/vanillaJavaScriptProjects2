613f8488a409347fe1d1a959c4345b04e40663e8
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-18 12:34:31 -0400

    add random user take 1 destrucutre and new module

--- 23-random-user/take1/app.js
+++ 23-random-user/take1/app.js
@@ -1,10 +1,19 @@
 import get from './utils/getElement.js';
-
-const url = 'https://randomuser.me/api/';
+import getUser from './utils/fetchUser.js';
 
 const img = get('.user-img');
 const title = get('.user-title');
 const value = get('.user-value');
 const btn = get('.btn');
 const btns = [...document.querySelectorAll('.icon')];
-console.log(btns);
+
+const showUser = async () => {
+  //get user from api
+  const person = await getUser();
+  console.log(person);
+  //display user info in btns
+};
+
+window.addEventListener('DOMContentLoaded', showUser);
+
+btn.addEventListener('click', showUser);
