056eda699d8faa800d5eefade2a6d2837624d64f
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-20 21:46:12 -0400

    add cocktails take 2 fetch drinks

--- 24-cocktails/take2/app.js
+++ 24-cocktails/take2/app.js
@@ -1,6 +1,6 @@
 import presentDrinks from './src/presentDrinks.js';
 
-const url = 'www.thecocktaildb.com/api/json/v1/1/search.php?s=a';
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a';
 
 window.addEventListener('DOMContentLoaded', () => {
   presentDrinks(url);
