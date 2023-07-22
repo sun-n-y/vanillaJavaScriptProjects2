2ff2a6bfda9cc943e6e012d8323f742abbbd31ad
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-22 18:08:49 -0400

    add take 3 loading

--- 24-cocktails/take3/src/displayDrinks.js
+++ 24-cocktails/take3/src/displayDrinks.js
@@ -1,10 +1,12 @@
 import get from './getElement.js';
+import { hideLoading } from './toggleLoading.js';
 
 const section = get('.section-center');
 const title = get('.title');
 
 const displayDrinks = ({ drinks }) => {
   if (!drinks) {
+    hideLoading();
     title.textContent = 'Sorry, error in fetch request';
     section.innerHTML = null;
     return;
@@ -14,12 +16,13 @@ const displayDrinks = ({ drinks }) => {
       const { idDrink: id, strDrinkThumb: image, strDrink: name } = drink;
       return `<a href="drink.html">
         <article class="cocktail data-id="${id}" ">
-          <img src="${image}" alt="${name}">
-          <h3>${name}</h3>
+        <img src="${image}" alt="${name}">
+        <h3>${name}</h3>
         </article>
-      </a>`;
+        </a>`;
     })
     .join('');
+  hideLoading();
   title.textContent = '';
   section.innerHTML = newDrink;
   return section;
---
+++ 24-cocktails/take3/src/toggleLoading.js
@@ -0,0 +1,11 @@
+import get from './getElement.js';
+
+const loading = get('.loading');
+
+export function showLoading() {
+  loading.classList.remove('hide-loading');
+}
+
+export function hideLoading() {
+  loading.classList.add('hide-loading');
+}
