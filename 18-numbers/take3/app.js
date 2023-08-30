0fc02d61016f30e29cd2e416184c3e6a52e39940
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-30 11:56:37 -0400

    add numbers take 3 js complete

--- 18-numbers/take3/app.js
+++ 18-numbers/take3/app.js
@@ -1,1 +1,21 @@
-console.log('numbers project');
+const spans = [...document.querySelectorAll('.number')];
+
+const updateCount = (el) => {
+  const value = parseInt(el.dataset.value);
+  const increment = Math.ceil(value / 1000);
+  let initialValue = 0;
+
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+    if (initialValue > value) {
+      el.textContent = `${value}+`;
+      clearInterval(increaseCount);
+      return;
+    }
+    el.textContent = `${initialValue}+`;
+  }, 1);
+};
+
+spans.forEach((span) => {
+  updateCount(span);
+});
