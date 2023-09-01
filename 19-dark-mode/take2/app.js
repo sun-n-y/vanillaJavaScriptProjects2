e798738cf3b8ef910674f7d47f017737f9870d2f
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-01 10:37:01 -0400

    add dark mode take js import data

--- 19-dark-mode/take2/app.js
+++ 19-dark-mode/take2/app.js
@@ -1,6 +1,23 @@
 //
 const toggleBtn = document.querySelector('.btn');
+const container = document.querySelector('.articles');
 
 toggleBtn.addEventListener('click', () => {
   document.documentElement.classList.toggle('dark-theme');
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  const articlesData = articles
+    .map(({ title, date, length, snippet }) => {
+      return `<article class="post">
+        <h2 class="post-title">${title}</h2>
+        <div class="post-info">
+          <span>${date}</span>
+          <span>${length}</span>
+        </div>
+        <p>${snippet}</p>
+      </article>`;
+    })
+    .join('');
+  container.innerHTML = articlesData;
 });
