4d21f1acc7ce70bca179af48a0ee022db25e2a0d
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-03 12:04:53 -0400

    add filters take 3 app js display products

--- 20-filters/take 3/app.js
+++ 20-filters/take 3/app.js
@@ -1,1 +1,21 @@
-console.log('filters project');
+//
+let filterdProducts = [...products];
+
+const productsContainer = document.querySelector('.products-container');
+
+const displayProducts = () => {
+  //if statement
+
+  productsContainer.innerHTML = filterdProducts
+    .map(({ id, title, image, price }) => {
+      return `<article class="product" data-id="${id}">
+        <img src="${image}" class="product-img img">
+        <footer>
+          <h5 class="product-name">${title}</h5>
+          <span class="product-price">$${price}</span>
+        </footer>
+      </article>`;
+    })
+    .join('');
+};
+displayProducts();
