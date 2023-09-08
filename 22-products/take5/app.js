8f387a2c68cd1c9d404aa83d49e3ccbb0d0e0c44
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-08 11:36:51 -0400

    add js load products

--- 22-products/take5/app.js
+++ 22-products/take5/app.js
@@ -17,7 +17,23 @@ const fetchProduct = async () => {
 };
 
 const displayProducts = (list) => {
-  console.log(list);
+  const productList = list
+    .map((product) => {
+      const { id } = product;
+      const { name: title, price } = product.fields;
+      const { url: img } = product.fields.image[0];
+      const formatPrice = price / 100;
+      return `<a href="product.html" class="single-product">
+          <img src="${img}" alt="${title}" class="single-product-img img">
+          <footer>
+            <h5 class="name">${title}</h5>
+            <span class="price">$${formatPrice}</span>
+          </footer>
+        </a>
+`;
+    })
+    .join('');
+  productDOM.innerHTML = `<div class="products-container">${productList}</div>`;
 };
 
 const start = async () => {
