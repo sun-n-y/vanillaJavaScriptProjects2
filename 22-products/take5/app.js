255b743ce635806d6b9ee0bb6bb81a92080300c4
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-08 11:04:58 -0400

    add js fetch products

--- 22-products/take5/app.js
+++ 22-products/take5/app.js
@@ -1,1 +1,15 @@
-console.log('products starter');
+//
+const url = 'https://course-api.com/javascript-store-products';
+const productDOM = document.querySelector('.products-center');
+
+const fetchProduct = async () => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchProduct();
