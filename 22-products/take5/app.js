c043e3b41902af2a948104d372420831378b99ff
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-08 11:10:29 -0400

    add js loading state error states'

--- 22-products/take5/app.js
+++ 22-products/take5/app.js
@@ -1,14 +1,18 @@
 //
-const url = 'https://course-api.com/javascript-store-products';
+const url = 'https://course-api.com/javascript-store-productsss';
 const productDOM = document.querySelector('.products-center');
 
 const fetchProduct = async () => {
+  productDOM.innerHTML = `<div class="loading"></div>`;
   try {
     const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('error');
+    }
     const data = await response.json();
     console.log(data);
   } catch (error) {
-    console.log(error);
+    productDOM.innerHTML = `<p class="error">there was an error...</p>`;
   }
 };
 
