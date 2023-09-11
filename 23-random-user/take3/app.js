7318b10bbf6ed47c251e870618ab0d6a1df550a4
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-11 10:55:36 -0400

    add random user module setup and api

---
+++ 23-random-user/take3/app.js
@@ -0,0 +1,12 @@
+//
+import get from './getElement.js';
+//
+const url = 'https://randomuser.me/api/';
+//
+const img = get('.user-img');
+const title = get('.user-title');
+const value = get('.user-value');
+const btn = get('.btn');
+const btns = [...document.querySelectorAll('.icon')];
+
+console.log(btns);
---
+++ 23-random-user/take3/getElement.js
@@ -0,0 +1,11 @@
+//helper fx
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  } else {
+    throw new Error(`"${selection}" selector does not exist!`);
+  }
+}
+
+export default getElement;
