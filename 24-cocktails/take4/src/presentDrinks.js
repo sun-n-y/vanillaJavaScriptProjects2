06ed94390af24b178682ee5b9786d40aa12ffea1
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-13 11:50:52 -0400

    add cocktails take4 js single drink html

--- 24-cocktails/take4/src/presentDrinks.js
+++ 24-cocktails/take4/src/presentDrinks.js
@@ -1,11 +1,14 @@
 import fetchDrinks from './fetchDrinks.js';
 import displayDrinks from './displayDrinks.js';
+import setDrink from './setDrink.js';
 
 const showDrinks = async (url) => {
   const data = await fetchDrinks(url);
 
   const section = await displayDrinks(data);
-  console.log(section);
+  if (section) {
+    setDrink(section);
+  }
 };
 
 export default showDrinks;
