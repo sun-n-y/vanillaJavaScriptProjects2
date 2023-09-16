72dc80a8b8abc5db1e95d7e900be4d7ee86d6d28
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-16 15:23:28 -0400

    add slider take1 js pt1

---
+++ 25-slider/take1/app.js
@@ -0,0 +1,30 @@
+//
+import people from './data.js';
+
+const container = document.querySelector('.slide-container');
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+
+//
+container.innerHTML = people
+  .map((person, slideIndex) => {
+    const { img, name, job, text } = person;
+    //
+    let position = 'next';
+    if (slideIndex === 0) {
+      position = 'active';
+    }
+    if (slideIndex === people.length - 1) {
+      position = 'last';
+    }
+    return `<article class="slide ${position}">
+        <img src="${img}" alt="${name}" class="img">
+        <h4>${name}</h4>
+        <p class="title">${job}</p>
+        <p class="text">${text}</p>
+        <div class="quote-icon">
+          <div class="fas fa-quote-right"></div>
+        </div>
+      </article>`;
+  })
+  .join('');
