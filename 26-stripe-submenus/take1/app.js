13147118a3e9787bde18b61bfada33e8a346eed0
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-19 11:50:10 -0400

    add stripe submenus take 1 sublinks

---
+++ 26-stripe-submenus/take1/app.js
@@ -0,0 +1,36 @@
+import sublinks from './data.js';
+
+const toggleBtn = document.querySelector('.toggle-btn');
+const closeBtn = document.querySelector('.close-btn');
+const sidebarWrapper = document.querySelector('.sidebar-wrapper');
+const sidebar = document.querySelector('.sidebar-links');
+const linkBtns = [...document.querySelectorAll('.link-btn')];
+const submenu = document.querySelector('.sub-menu');
+const hero = document.querySelector('.hero');
+const nav = document.querySelector('.nav');
+
+//hide/show sidebar
+toggleBtn.addEventListener('click', () => {
+  sidebarWrapper.classList.add('show');
+});
+closeBtn.addEventListener('click', () => {
+  sidebarWrapper.classList.remove('show');
+});
+
+//set sidebar
+sidebar.innerHTML = sublinks
+  .map((item) => {
+    const { links, page } = item;
+    return `<article>
+  <h4>${page}</h4>
+  <div class="sidebar-sublinks">
+  ${links
+    .map((link) => {
+      return `<a href="${link.url}"><i class="${link.icon}"></i>${link.label}
+      </a>`;
+    })
+    .join('')}
+  </div>
+  </article>`;
+  })
+  .join('');
