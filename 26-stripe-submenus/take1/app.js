09f18ca4725355fe1f0e5f1828e7fa8764932e3d
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-19 12:41:11 -0400

    add stripe submenus take 1 complete

--- 26-stripe-submenus/take1/app.js
+++ 26-stripe-submenus/take1/app.js
@@ -52,10 +52,18 @@ linkBtns.forEach((btn) => {
       submenu.classList.add('show');
       submenu.style.left = `${center}px`;
       submenu.style.top = `${bottom}px`;
+      //columns
+      let columns = 'col-2';
+      if (links.length === 3) {
+        columns = 'col-3';
+      }
+      if (links.length > 3) {
+        columns = 'col-4';
+      }
       submenu.innerHTML = `
       <section>
       <h4>${page}</h4>
-      <div class="submenu-center col-2">
+      <div class="submenu-center ${columns}">
         ${links
           .map((link) => {
             return `<a href="${link.url}"><i class="${link.icon}"></i>${link.label}</a>`;
@@ -66,4 +74,14 @@ linkBtns.forEach((btn) => {
       `;
     }
   });
+});
+
+//hide submenu
+hero.addEventListener('mouseover', function (e) {
+  submenu.classList.remove('show');
+});
+nav.addEventListener('mouseover', function (e) {
+  if (!e.target.classList.contains('link-btn')) {
+    submenu.classList.remove('show');
+  }
 });
