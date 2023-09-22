f07e0e4ec5312cca2ad0e6c0abfe86362c4ddc67
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-22 10:49:24 -0400

    add pagination take1 display followers

--- 27-pagination/take1/app.js
+++ 27-pagination/take1/app.js
@@ -3,9 +3,12 @@ import displayFollowers from './displayFollowers.js';
 import paginate from './paginate.js';
 import displayButtons from './displayButtons.js';
 
+const title = document.querySelector('.section-title h1');
+
 const init = async () => {
   const followers = await fetchFollowers();
-  console.log(followers);
+  displayFollowers(followers);
+  title.textContent = 'pagination';
 };
 
 window.addEventListener('load', init);
