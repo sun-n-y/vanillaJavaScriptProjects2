34bb34a730e49d92a1178bc0a2d5e70cb41022de
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-24 12:01:42 -0400

    add pagination take 2 fetch followers

--- 27-pagination/take2/fetchFollowers.js
+++ 27-pagination/take2/fetchFollowers.js
@@ -1,3 +1,9 @@
-const fetchFollowers = async () => {};
+const url = 'https://api.github.com/users/john-smilga/followers?per_page=100';
+
+const fetchFollowers = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
 
 export default fetchFollowers;
