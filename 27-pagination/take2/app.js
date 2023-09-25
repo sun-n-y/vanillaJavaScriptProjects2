6fcecdd24a59592d086fc5b14ee15fdb93d57763
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-25 16:49:46 -0400

    add paginate take 2 paginate js

--- 27-pagination/take2/app.js
+++ 27-pagination/take2/app.js
@@ -7,8 +7,10 @@ const title = document.querySelector('.section-title h1');
 
 const init = async () => {
   const followers = await fetchFollowers();
-  displayFollowers(followers);
+  displayFollowers(paginate(followers)[0]);
   title.textContent = 'pagination';
+  const pages = paginate(followers);
+  console.log(pages);
 };
 
 window.addEventListener('load', init);
--- 27-pagination/take2/paginate.js
+++ 27-pagination/take2/paginate.js
@@ -1,3 +1,11 @@
-const paginate = () => {};
+const paginate = (followers) => {
+  const itemsPerPage = 9;
+  const numeberOfPages = Math.ceil(followers.length / itemsPerPage);
+  const newFollowers = Array.from({ length: numeberOfPages }, (_, index) => {
+    const start = index * itemsPerPage;
+    return followers.slice(start, start + itemsPerPage);
+  });
+  return newFollowers;
+};
 
 export default paginate;
