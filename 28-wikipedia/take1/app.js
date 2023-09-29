19f1ce3ace2a2c5358e2f4022d6fcfe2d9f4a267
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-29 10:39:34 -0400

    add wikipedia take1 app js intial setup

--- 28-wikipedia/take1/app.js
+++ 28-wikipedia/take1/app.js
@@ -1,2 +1,21 @@
 const url =
   'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=searchValue';
+
+const formDOM = document.querySelector('.form');
+const inputDOM = document.querySelector('.form-input');
+const resultsDOM = document.querySelector('.results');
+
+formDOM.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const value = inputDOM.value;
+  if (!value) {
+    resultsDOM.innerHTML =
+      '<div class="error">please enter vaild search term</div>';
+    return;
+  }
+  fetchPages(value);
+});
+
+const fetchPages = async (searchValue) => {
+  console.log(searchValue);
+};
