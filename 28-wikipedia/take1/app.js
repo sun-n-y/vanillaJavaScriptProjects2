53c7b8e28a366ae736e6dfc58883f67c7bce1631
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-29 10:58:29 -0400

    add wikipedia take1 app js fetch pages

--- 28-wikipedia/take1/app.js
+++ 28-wikipedia/take1/app.js
@@ -1,5 +1,5 @@
 const url =
-  'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=searchValue';
+  'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=';
 
 const formDOM = document.querySelector('.form');
 const inputDOM = document.querySelector('.form-input');
@@ -17,5 +17,19 @@ formDOM.addEventListener('submit', (e) => {
 });
 
 const fetchPages = async (searchValue) => {
-  console.log(searchValue);
+  resultsDOM.innerHTML = '<div class="loading"></div>';
+  try {
+    const response = await fetch(`${url}${searchValue}`);
+    const data = await response.json();
+    const results = data.query.search;
+    if (results.length < 1) {
+      resultsDOM.innerHTML =
+        '<div class="error">no matching results. please try again.</div>';
+    }
+    rederResults(results);
+  } catch (error) {}
+};
+
+const rederResults = (list) => {
+  console.log(list);
 };
