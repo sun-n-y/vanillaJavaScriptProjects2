ce96a9999e2c33e611ed2b2853c6924660f17d42
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-03 10:46:11 -0400

    add comfy store take 1 format price

--- 29-comfy-store/take1/src/displayProducts.js
+++ 29-comfy-store/take1/src/displayProducts.js
@@ -17,7 +17,7 @@ const display = (products, element) => {
         </div>
         <footer>
           <p class="product-name">${name}</p>
-          <h4 class="product-price">${price}</h4>
+          <h4 class="product-price">${formatPrice(price)}</h4>
         </footer>
       </article>`;
     })
