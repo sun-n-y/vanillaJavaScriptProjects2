25d4aeaa338acf491f45c05bfc1b42d4a66b21a9
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-03 10:13:21 -0400

    add comfy store take 1 local storage setup

--- 29-comfy-store/take1/src/store.js
+++ 29-comfy-store/take1/src/store.js
@@ -1,5 +1,5 @@
 import { getStorageItem, setStorageItem } from './utils.js';
-let store = [];
+let store = getStorageItem('store');
 const setupStore = (products) => {
   store = products.map((product) => {
     const {
@@ -9,6 +9,8 @@ const setupStore = (products) => {
     const image = img[0].thumbnails.large.url;
     return { id, featured, name, price, colors, company, image };
   });
+  setStorageItem('store', store);
 };
+console.log(store);
 const findProduct = () => {};
 export { store, setupStore, findProduct };
