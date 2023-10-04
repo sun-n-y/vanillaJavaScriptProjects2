945ff4c88d6fa2ac8d734530d60256918ce0dd57
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-04 14:46:50 -0400

    add comfy store take 1 single product js complete

--- 29-comfy-store/take1/src/pages/product.js
+++ 29-comfy-store/take1/src/pages/product.js
@@ -28,7 +28,23 @@ window.addEventListener('DOMContentLoaded', async () => {
     const response = await fetch(`${singleProductUrl}${urlID}`);
     if (response.status >= 200 && response.status <= 299) {
       const product = await response.json();
-      console.log(product);
+      const { id, fields } = product;
+      productID = id;
+      const { name, company, price, colors, description } = fields;
+      const image = fields.image[0].thumbnails.large.url;
+      window.document.title = `${name.toUpperCase()} | Comfy`;
+      pageTitleDOM.textContent = `Home / ${name}`;
+      imgDOM.src = image;
+      titleDOM.textContent = name;
+      companyDOM.textContent = `by ${company}`;
+      priceDOM.textContent = formatPrice(price);
+      descDOM.textContent = description;
+      colors.forEach((color) => {
+        const span = document.createElement('span');
+        span.classList.add('product-color');
+        span.style.backgroundColor = `${color}`;
+        colorsDOM.appendChild(span);
+      });
     } else {
       console.log(response.status, response.statusText);
       centerDOM.innerHTML = `
@@ -42,4 +58,8 @@ window.addEventListener('DOMContentLoaded', async () => {
     console.log(error);
   }
   loading.style.display = 'none';
+});
+
+cartBtn.addEventListener('click', function () {
+  addToCart(productID);
 });
