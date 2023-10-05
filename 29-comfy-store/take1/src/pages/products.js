ea4381fd1f48662701976b7d22ce1be8685ec378
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-05 15:08:17 -0400

    add comfy store take 1 add bug fix 2

--- 29-comfy-store/take1/src/pages/products.js
+++ 29-comfy-store/take1/src/pages/products.js
@@ -9,15 +9,27 @@ import setupCompanies from '../filters/companies.js';
 import setupPrice from '../filters/price.js';
 
 // specific imports
-import { store } from '../store.js';
+import { store, setupStore } from '../store.js';
 import display from '../displayProducts.js';
 import { getElement } from '../utils.js';
 
-const loading = getElement('.page-loading');
+//impot fetch products
+import fetchProducts from '../fetchProducts.js';
 
-display(store, getElement('.products-container'));
-setupSearch(store);
-setupCompanies(store);
-setupPrice(store);
+const init = async () => {
+  const loading = getElement('.page-loading');
 
-loading.style.display = 'none';
+  if (store.length < 1) {
+    const products = await fetchProducts();
+    setupStore(products);
+  }
+
+  display(store, getElement('.products-container'));
+  setupSearch(store);
+  setupCompanies(store);
+  setupPrice(store);
+
+  loading.style.display = 'none';
+};
+
+init();
