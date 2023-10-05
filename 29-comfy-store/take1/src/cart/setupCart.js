731690d4c0006ec2004fba0d2dde36a5ff5274e5
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-05 10:51:22 -0400

    add comfy store take 1 cart setup part 1

--- 29-comfy-store/take1/src/cart/setupCart.js
+++ 29-comfy-store/take1/src/cart/setupCart.js
@@ -10,7 +10,18 @@ import { findProduct } from '../store.js';
 import addToCartDOM from './addToCartDOM.js';
 // set items
 
+const cartItemCountDOM = getElement('.cart-item-count');
+const cartItemsDOM = getElement('.cart-items');
+const cartTotalDOM = getElement('.cart-total');
+
+let cart = getStorageItem('cart');
+
 export const addToCart = (id) => {
   console.log(id);
   openCart();
 };
+
+const init = () => {
+  console.log(cart);
+};
+init();
